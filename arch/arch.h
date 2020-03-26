e264a98802844551a81b55cbfc8d52d3f91e88c3
Kyle Genoe <kyle@genoe.ca>
2020-03-26 12:30:43 -0400

    Initial Commit

---
+++ arch/arch.h
@@ -0,0 +1,19 @@
+//
+//  arch.h
+//  arch
+//
+//  Created by Kyle Genoe on 2020-03-26.
+//  Copyright © 2020 Kyle Genoe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for arch.
+FOUNDATION_EXPORT double archVersionNumber;
+
+//! Project version string for arch.
+FOUNDATION_EXPORT const unsigned char archVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <arch/PublicHeader.h>
+
+
